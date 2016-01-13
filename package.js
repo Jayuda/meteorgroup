57d2e5118c6fe74fdc9d61837582a7c3dd8784d0
Pamungkas Jayuda <yulius.jayuda@gmail.com>
2016-01-13 15:43:10 +0700

    update client side package

--- package.js
+++ package.js
@@ -1,15 +1,14 @@
 Package.describe({
   name: 'jayuda:meteorgroup',
-  version: '0.0.1',
+  version: '0.0.4',
   summary: 'Reactive group to sum / min / max / avg data in MiniMongo - client side.',
   git: 'https://github.com/Jayuda/meteorgroup',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  api.versionsFrom('1.2.1', 'client');
   api.use('mongo', 'client')
   api.use('reactive-var', 'client')
-  api.use('ecmascript');
-  api.addFiles('meteorgroup.js');
+  api.addFiles('meteorgroup.js', 'client');
 });
